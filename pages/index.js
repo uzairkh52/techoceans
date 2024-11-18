41bf8dc813b111247dafa71404d35b8ddbe113fa
muhammad uzair khan <uzairkh52@gmail.com>
2024-11-18 20:49:08 +0500

    222

--- pages/index.js
+++ pages/index.js
@@ -26,6 +26,7 @@ export default function Home() {
           height={38}
           priority
         />
+        <h1>test</h1>
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}

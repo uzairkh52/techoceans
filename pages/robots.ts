8bd3187cf75124260db557a5a2f13fba8e0cb254
muhammad uzair khan <uzairkh52@gmail.com>
2024-11-23 04:41:28 +0500

    555

--- pages/robots.ts
+++
@@ -1,7 +0,0 @@
-const robotsTxtConfig = {
-  rules: [
-    { userAgent: '*', allow: '/' },
-    { userAgent: 'Googlebot', disallow: '/private' },
-  ],
-  sitemap: 'https://techoceans.vercel.app/sitemap.xml',
-};

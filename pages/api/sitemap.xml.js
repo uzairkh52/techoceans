736105a05b760bb00a5ea15f105a88b748599e57
muhammad uzair khan <uzairkh52@gmail.com>
2024-11-23 19:15:29 +0500

    666

--- pages/api/sitemap.xml.js
+++ pages/api/sitemap.xml.js
@@ -3,13 +3,13 @@ export default function handler(req, res) {
    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
        <url>
-           <loc>techoceans.vercel.app/</loc>
+           <loc>https://techoceans.vercel.app/</loc>
            <lastmod>2024-11-22</lastmod>
            <changefreq>daily</changefreq>
            <priority>1.0</priority>
        </url>
        <url>
-           <loc>techoceans.vercel.app/about/</loc>
+           <loc>https://techoceans.vercel.app/about/</loc>
            <lastmod>2024-11-22</lastmod>
            <changefreq>daily</changefreq>
            <priority>2.0</priority>

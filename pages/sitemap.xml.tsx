d6ab348957c4de7e207fd8c8b366737617b4d0cc
muhammad uzair khan <uzairkh52@gmail.com>
2024-11-23 11:25:51 +0500

    666

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -6,9 +6,15 @@ function generateSiteMap(posts) {
    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
      <!--We manually set the two URLs we know already-->
      <url>
-       <loc>https://techoceans.vercel.app</loc>
-       <loc>https://techoceans.vercel.app/about</loc>
-     </url>
+      <loc>https://techoceans.vercel.app/</loc>
+      <lastmod>2024-11-22</lastmod>
+      <priority>1.0</priority>
+   </url>
+   <url>
+      <loc>https://techoceans.vercel.app/about</loc>
+      <lastmod>2024-11-20</lastmod>
+      <priority>0.8</priority>
+   </url>
      
    </urlset>
  `;

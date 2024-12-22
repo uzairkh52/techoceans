f8cee6cf7628e6e21ba45cfd28288d72b5b23ee6
muhammad uzair khan <uzairkh52@gmail.com>
2024-12-22 17:20:54 +0500

    blog slug

--- pages/api/robots.txt.js
+++ pages/api/robots.txt.js
@@ -1,12 +1,8 @@
 export default function handler(req, res) {
-   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'https://techoceans.vercel.app/';
-
    res.setHeader('Content-Type', 'text/plain');
    res.status(200).send(`
-User-agent: *
-Allow: /
-
-
-Sitemap: ${baseUrl}/sitemap.xml
+     User-agent: *
+     Allow: /
    `);
-}
+ }
+ 

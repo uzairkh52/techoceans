99d2432678d5b5af4b7e34be5934882d03a86aa7
muhammad uzair khan <uzairkh52@gmail.com>
2025-01-06 13:24:59 +0500

    sitemap

--- component/Header.js
+++ component/Header.js
@@ -26,6 +26,8 @@ const Header = () => {
   return (
     <>
       <Head>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1624182310549814"
+     crossorigin="anonymous"></script>
         {/* <!-- Google tag (gtag.js) --> */}
         
 
